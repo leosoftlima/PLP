b5ab5959b38b6f60c8bbb9ea3741dc3b3f921d44
unknown <fehaa2000@gmail.com>
2020-04-29 15:39:00 -0300

    Adicionando implementações de != e módulo em le2

---
+++ Expressoes2/src/le2/plp/expressions2/expression/ExpRem.java
@@ -0,0 +1,35 @@
+package le2.plp.expressions2.expression;
+
+import le2.plp.expressions1.util.Tipo;
+import le2.plp.expressions1.util.TipoPrimitivo;
+import le2.plp.expressions2.memory.AmbienteCompilacao;
+import le2.plp.expressions2.memory.AmbienteExecucao;
+import le2.plp.expressions2.memory.VariavelJaDeclaradaException;
+import le2.plp.expressions2.memory.VariavelNaoDeclaradaException;
+
+public class ExpRem extends ExpBinaria {
+
+	public ExpRem(Expressao esq, Expressao dir) {
+		super(esq, dir, "%");
+	}
+
+	public Valor avaliar(AmbienteExecucao amb) throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
+		return new ValorInteiro(
+				((ValorInteiro) getEsq().avaliar(amb)).valor() % ((ValorInteiro) getDir().avaliar(amb)).valor());
+	}
+
+	public Tipo getTipo(AmbienteCompilacao amb) throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
+		return TipoPrimitivo.INTEIRO;
+	}
+
+	protected boolean checaTipoElementoTerminal(AmbienteCompilacao ambiente)
+			throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
+		return (getEsq().getTipo(ambiente).eInteiro() && getDir().getTipo(ambiente).eInteiro());
+	}
+
+	@Override
+	public ExpBinaria clone() {
+		return new ExpRem(esq.clone(), dir.clone());
+	}
+
+}
