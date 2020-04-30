13abb8f68cf329b2645f9f67f1d35f5dede2c344
unknown <fehaa2000@gmail.com>
2020-04-30 16:34:37 -0300

    Adicionando implementações de != e módulo em li2

---
+++ Imperativa2/src/li2/plp/expressions2/expression/ExpNotEquals.java
@@ -0,0 +1,36 @@
+package li2.plp.expressions2.expression;
+
+import li2.plp.expressions1.util.Tipo;
+import li2.plp.expressions1.util.TipoPrimitivo;
+import li2.plp.expressions2.memory.AmbienteCompilacao;
+import li2.plp.expressions2.memory.AmbienteExecucao;
+import li2.plp.expressions2.memory.VariavelJaDeclaradaException;
+import li2.plp.expressions2.memory.VariavelNaoDeclaradaException;
+
+public class ExpNotEquals extends ExpBinaria {
+
+	public ExpNotEquals(Expressao esq, Expressao dir) {
+		super(esq, dir, "!=");
+	}
+
+	public Valor avaliar(AmbienteExecucao amb) throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
+		ValorConcreto esq = (ValorConcreto) getEsq().avaliar(amb);
+		ValorConcreto dir = (ValorConcreto) getDir().avaliar(amb);
+		return new ValorBooleano(!esq.isEquals(dir));
+	}
+
+	public Tipo getTipo(AmbienteCompilacao amb) throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
+		return TipoPrimitivo.BOOLEANO;
+	}
+
+	protected boolean checaTipoElementoTerminal(AmbienteCompilacao amb)
+			throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
+		return getEsq().getTipo(amb).eIgual(getDir().getTipo(amb));
+	}
+
+	@Override
+	public ExpBinaria clone() {
+		return new ExpNotEquals(esq.clone(), dir.clone());
+	}
+
+}
