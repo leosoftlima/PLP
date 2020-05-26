4da06e585ada74c8fe001a6c7b6c54d6ab5e466d
unknown <fehaa2000@gmail.com>
2020-05-26 20:35:13 -0300

    Adicionando texto do átomo AssignmentAsValue alterado

--- Imperativa2/src/li2/plp/imperative1/command/Atribuicao.java
+++ Imperativa2/src/li2/plp/imperative1/command/Atribuicao.java
@@ -1,7 +1,10 @@
 package li2.plp.imperative1.command;
 
 import li2.plp.expressions2.expression.Expressao;
+import li2.plp.expressions2.expression.Id;
 
 public interface Atribuicao extends Comando {
+	Id getId();
+
 	Expressao getExpressao();
 }
