e3e379dda1e5b1065e0af5a86aa5f51fd7da9fb0
unknown <fehaa2000@gmail.com>
2020-05-26 20:32:38 -0300

    Adicionando separador de linha no fim de cada novo append

--- Imperativa2/src/li2/plp/imperative2/util/Logger.java
+++ Imperativa2/src/li2/plp/imperative2/util/Logger.java
@@ -1,26 +1,29 @@
 package li2.plp.imperative2.util;
 
 public class Logger {
-	
-	private final static Logger instance = new Logger();
-	
-	private StringBuilder mensagem = new StringBuilder();
-	  
-    private Logger() {}
 
-    public static Logger getInstance() {
-              return instance;
-    }
+	private final static Logger instance = new Logger();
+
+	private StringBuilder mensagem = new StringBuilder();
+
+	private Logger() {
+	}
+
+	public static Logger getInstance() {
+		return instance;
+	}
 
 	public StringBuilder append(String str) {
-		return mensagem.append(str);
+		return mensagem.append(str + System.getProperty("line.separator"));
 	}
-	
+
 	public StringBuilder getMensagem() {
 		return mensagem;
 	}
+
 	public void limparMensagem() {
-		this.mensagem.setLength(0);  ;
+		this.mensagem.setLength(0);
+		;
 	}
-	
+
 }
